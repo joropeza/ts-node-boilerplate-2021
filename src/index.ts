45bd182d7553249fcfec8d612982112f82fd7f58
Jon Oropeza <jonny@Jons-MacBook-Pro.local>
2021-01-25 21:35:06 -0800

    add tsc build config

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,11 @@ require('dotenv').config(); // https://www.npmjs.com/package/dotenv
 (async () => {
   console.log('yo.');
 })();
+
+// PLAN
+
+// Read from Evernote API
+
+// Write to compiler's file storage
+
+// Post to slackbot
