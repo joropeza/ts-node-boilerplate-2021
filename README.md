ebbd427a669a9ac2ba04b03955ce2e2ceb3797de
Jon Oropeza <jonny@Jons-MacBook-Pro.local>
2021-01-25 21:40:42 -0800

    add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Jonny's 2021 TypeScript Boilerplate
+
+A highly opinionated boilerplate for TypeScript + Node.js apps. 
+
+You probably won't like this.
--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,3 @@ require('dotenv').config(); // https://www.npmjs.com/package/dotenv
 (async () => {
   console.log('yo.');
 })();
-
-// PLAN
-
-// Read from Evernote API
-
-// Write to compiler's file storage
-
-// Post to slackbot
