d115da6f5a8173e584894e6d040f4e9721bb97f9
Jon Oropeza <joropeza@gmail.com>
2021-01-26 20:17:15 -0800

    add some sample code

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
 require('dotenv').config(); // https://www.npmjs.com/package/dotenv
 
+const moduleFunction = async (runSuccessfully: boolean) => {
+  if (runSuccessfully) {
+    return 'yo';
+  }
+  throw new Error('whoops');
+};
+
 (async () => {
-  console.log('yo.');
-})();
+  const response = await moduleFunction(true);
+  console.log(response);
+})().catch((e: Error) => {
+  console.error(e);
+  process.exit(1);
+});
