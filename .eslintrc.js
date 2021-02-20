3c71019f3fe2ddb560d8d475e076063e1fdf887c
Jon Oropeza <joropeza@gmail.com>
2021-02-20 10:54:16 -0800

    allow console and ad hoc exports

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,9 @@ module.exports = {
     extends: ['airbnb-typescript/base'],
     parserOptions: {
         project: './tsconfig.json',
+    },
+    rules: {
+        'no-console': 'off',
+        'import/prefer-default-export': 'off'
     }
 };
